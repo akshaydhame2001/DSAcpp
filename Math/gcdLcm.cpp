08f3e53ed2a78a4da043bd75d17f970a9231c73a
Harshal <akshaydhame2001@gmail.com>
2025-01-21 14:59:11 +0530

    commit

---
+++ Math/gcdLcm.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> lcmAndGcd(int a, int b)
+{
+    // code here
+    int gcd = 1;
+    int n1 = a;
+    int n2 = b;
+    while (a > 0 && b > 0)
+    {
+        if (a > b)
+        {
+            a = a % b;
+        }
+        else
+        {
+            b = b % a;
+        }
+    }
+    if (a == 0)
+    {
+        gcd = b;
+    }
+    else if (b == 0)
+    {
+        gcd = a;
+    }
+    int lcm = (n1 * n2) / gcd;
+    return {lcm, gcd};
+}
+
+int main()
+{
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "[LCM, GCD]: ";
+    cout << "[" << lcmAndGcd(a, b)[0] << "," << lcmAndGcd(a, b)[1] << "]" << endl;
+
+    return 0;
+}
