c375dd1314106a5375134e2f727986c6d28475d5
Harshal <akshaydhame2001@gmail.com>
2025-01-29 16:02:19 +0530

    commit

---
+++ hashing/hash1.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter array size: ";
+    cin >> n;
+    int arr[n];
+    cout << endl
+         << "Enter array elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // precompute
+    int hash[13] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        hash[arr[i]] += 1;
+    }
+
+    int q;
+    cout << "Number of queries: ";
+    cin >> q;
+    cout << "Queries: ";
+    while (q--)
+    {
+        int number;
+        cin >> number;
+        cout << hash[number] << endl;
+    }
+
+    return 0;
+}
