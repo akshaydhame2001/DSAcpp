f7e6871fb46a5d5486f256f73f8e5855c08025b5
Harshal <akshaydhame2001@gmail.com>
2025-02-27 10:57:39 +0530

    commit

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -1,23 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-//  Divide ad merge
-void merge_sort(int arr[], int n)
+
+void merge(vector<int> &arr, int low, int mid, int high)
 {
+    vector<int> temp;
+    int left = low, right = mid + 1;
+
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+            temp.push_back(arr[left++]);
+        else
+            temp.push_back(arr[right++]);
+    }
+
+    while (left <= mid)
+        temp.push_back(arr[left++]);
+    while (right <= high)
+        temp.push_back(arr[right++]);
+
+    for (int i = low; i <= high; i++)
+        arr[i] = temp[i - low];
+}
+
+void merge_sort(vector<int> &arr, int low, int high)
+{
+    if (low >= high)
+        return;
+
+    int mid = low + (high - low) / 2;
+    merge_sort(arr, low, mid);
+    merge_sort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
 }
 
 int main()
 {
     int n;
     cin >> n;
-    int arr[n];
+    vector<int> arr(n);
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-    merge_sort(arr, n);
+
+    merge_sort(arr, 0, n - 1);
+
     for (int i = 0; i < n; i++)
-    {
         cout << arr[i] << " ";
-    }
+
     return 0;
 }
 
-// time complexity: O(Logn)
+// Time Complexity: O(n log n)
