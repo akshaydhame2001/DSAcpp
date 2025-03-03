6dc28032985c2826d0f138ff02abcbdbb0ed2214
Harshal <akshaydhame2001@gmail.com>
2025-03-03 14:40:39 +0530

    AWS Sagemaker

--- Cloud/sagemaker_test.py
+++ Cloud/sagemaker_test.py
@@ -39,3 +39,224 @@ sns.pairplot(
    vars=["Area", "MajorAxisLength", "MinorAxisLength", "Eccentricity", "roundness"],
    hue="Class",
 )
+
+# Plotting Correlation matrix
+# +1 Red related features, -1 opposite features, dia always +1 self-relation
+import matplotlib.pyplot as plt
+
+correlation = dry_bean.corr(numeric_only=True)
+
+# Create a square heatmap with center at 0
+sns.heatmap(correlation, center=0, square=True, cmap="coolwarm", vmin=-1, vmax=1)
+plt.show()
+
+# Preparing data for model training
+# encoding using sklearn as it is text
+from sklearn.preprocessing import LabelEncoder
+
+# For preprocessing
+df = dry_bean.copy(deep=True)
+
+# Encode the target
+le = LabelEncoder()
+df[TARGET_NAME] = le.fit_transform(df[TARGET_NAME])
+
+from sklearn.model_selection import train_test_split
+
+# Split the data into two sets
+train, test = train_test_split(df, random_state=1, test_size=0.2)
+
+train.to_csv("dry-bean-train.csv")
+test.to_csv("dry-bean-test.csv")
+
+# Send data to S3. SageMaker will take training data from s3
+trainpath = sess.upload_data(
+   path="dry-bean-train.csv",
+   bucket=BUCKET_NAME,
+   key_prefix="sagemaker/sklearncontainer",
+)
+
+testpath = sess.upload_data(
+   path="dry-bean-test.csv",
+   bucket=BUCKET_NAME,
+   key_prefix="sagemaker/sklearncontainer",
+)
+
+# Training script for SageMaker
+# %%writefile script.py
+
+import argparse
+import os
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import balanced_accuracy_score
+
+if __name__ == "__main__":
+   print("extracting arguments")
+   parser = argparse.ArgumentParser()
+
+   # Hyperparameters sent by the client are passed as command-line arguments to the script.
+   parser.add_argument("--n-estimators", type=int, default=10)
+   parser.add_argument("--min-samples-leaf", type=int, default=3)
+
+   # Data, model, and output directories
+   parser.add_argument("--model-dir", type=str, default=os.environ.get("SM_MODEL_DIR"))
+   parser.add_argument("--train", type=str, default=os.environ.get("SM_CHANNEL_TRAIN"))
+   parser.add_argument("--test", type=str, default=os.environ.get("SM_CHANNEL_TEST"))
+   parser.add_argument("--train-file", type=str, default="dry-bean-train.csv")
+   parser.add_argument("--test-file", type=str, default="dry-bean-test.csv")
+   args, _ = parser.parse_known_args()
+   
+   print("reading data")
+
+   train_df = pd.read_csv(os.path.join(args.train, args.train_file))
+   test_df = pd.read_csv(os.path.join(args.test, args.test_file))
+
+   print("building training and testing datasets")
+
+   X_train = train_df.drop("Class", axis=1)
+   X_test = test_df.drop("Class", axis=1)
+   y_train = train_df[["Class"]]
+   y_test = test_df[["Class"]]
+
+   # Train model
+   print("training model")
+
+   model = RandomForestClassifier(
+       n_estimators=args.n_estimators,
+       min_samples_leaf=args.min_samples_leaf,
+       n_jobs=-1,
+   )
+
+   model.fit(X_train, y_train)
+
+   # Print abs error
+   print("validating model")
+
+   bal_acc_train = balanced_accuracy_score(y_train, model.predict(X_train))
+   bal_acc_test = balanced_accuracy_score(y_test, model.predict(X_test))
+
+   print(f"Train balanced accuracy: {bal_acc_train:.3f}")
+   print(f"Test balanced accuracy: {bal_acc_test:.3f}")
+
+   # Persist model
+   path = os.path.join(args.model_dir, "model.joblib")
+   joblib.dump(model, path)
+   print("model persisted at " + path)
+
+# Check Working
+# ! python script.py --n-estimators 100 \
+
+#                   --min-samples-leaf 2 \
+
+#                   --model-dir ./ \
+
+#                   --train ./ \
+
+#                   --test ./ \
+
+# Custome training Job
+# We use the Estimator from the SageMaker Python SDK
+from sagemaker.sklearn.estimator import SKLearn
+FRAMEWORK_VERSION = "0.23-1"
+
+sklearn_estimator = SKLearn(
+   entry_point="script.py",
+   role=get_execution_role(),
+   instance_count=1,
+   instance_type="ml.c5.xlarge",
+   framework_version=FRAMEWORK_VERSION,
+   base_job_name="rf-scikit",
+   hyperparameters={
+       "n-estimators": 100,
+       "min-samples-leaf": 3,
+   },
+)
+# Launch training job, with asynchronous call
+sklearn_estimator.fit({"train": trainpath, "test": testpath}, wait=True)
+
+
+# Spot instance o reduce cost
+spot_sklearn_estimator = SKLearn(
+   entry_point="script.py",
+   role=get_execution_role(),
+   instance_count=1,
+   instance_type="ml.c5.xlarge",
+   framework_version=FRAMEWORK_VERSION,
+   base_job_name="rf-scikit",
+   hyperparameters={
+       "n-estimators": 100,
+       "min-samples-leaf": 3,
+   },
+   use_spot_instances=True,
+   max_wait=7200,
+   max_run=3600,
+)
+# Launch training job, with asynchronous call
+spot_sklearn_estimator.fit({"train": trainpath, "test": testpath}, wait=True)
+
+# Hyperparameter Training
+from sagemaker.tuner import IntegerParameter
+# We use the Hyperparameter Tuner
+from sagemaker.tuner import IntegerParameter
+
+# Define exploration boundaries
+hyperparameter_ranges = {
+   "n-estimators": IntegerParameter(20, 100),
+   "min-samples-leaf": IntegerParameter(2, 6),
+}
+
+# Create Optimizer
+Optimizer = sagemaker.tuner.HyperparameterTuner(
+   estimator=sklearn_estimator,
+   hyperparameter_ranges=hyperparameter_ranges,
+   base_tuning_job_name="RF-tuner",
+   objective_type="Maximize",
+   objective_metric_name="balanced-accuracy",
+   metric_definitions=[
+       {"Name": "balanced-accuracy", "Regex": "Test balanced accuracy: ([0-9.]+).*$"}
+   ],  # Extract tracked metric from logs with regexp
+   max_jobs=10,
+   max_parallel_jobs=2,
+)
+print(f"Train balanced accuracy: {bal_acc_train:.3f}")
+print(f"Test balanced accuracy: {bal_acc_test:.3f}")
+{
+   "Name": "balanced-accuracy",
+   "Regex": "Test balanced accuracy: ([0-9.]+).*$",
+}
+Optimizer.fit({"train": trainpath, "test": testpath})
+# Get tuner results in a df
+results = Optimizer.analytics().dataframe()
+
+while results.empty:
+   time.sleep(1)
+   results = Optimizer.analytics().dataframe()
+
+results.head()
+best_estimator = Optimizer.best_estimator()
+
+
+# Deployeing models as endpoints 
+artifact_path = sm_boto3.describe_training_job(
+   TrainingJobName=best_estimator.latest_training_job.name
+)["ModelArtifacts"]["S3ModelArtifacts"]
+
+print("Model artifact persisted at " + artifact)
+
+from sagemaker.sklearn.model import SKLearnModel
+
+model = SKLearnModel(
+   model_data=artifact_path,
+   role=get_execution_role(),
+   entry_point="script.py",
+   framework_version=FRAMEWORK_VERSION,
+)
+
+preds = predictor.predict(test.sample(4).drop("Class", axis=1))
+print(preds)
+
+# delete to stop the cost
+sm_boto3.delete_endpoint(EndpointName=predictor.endpoint)
